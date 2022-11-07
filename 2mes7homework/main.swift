bf045513d52b1f062d0d3e2dcd5d860f46259cf6
safarliev <nurikonurik@gmail.com>
2022-11-07 16:21:46 +0600

    done

--- 2mes7homework/main.swift
+++ 2mes7homework/main.swift
@@ -7,5 +7,46 @@
 
 import Foundation
 
-print("Hello, World!")
+// 1
+var numbers = [1, 2, 2.5, 3, 4.4]
+
+extension Array {
+    func square<T: Numeric>(array: [T]){
+        for i in array{
+            print(i * i)
+            }
+            
+    }
+}
+
+numbers.square(array: numbers)
+
+// 2
+
+struct Searchitem<T> {
+    
+    var array: [T]
+    
+    func search(item: T) {
+        for i in array{
+            if i == item {
+                print("Item was founded")
+                break
+            }
+        }
+        print("Item wasn't founded")
+    }
+    
+}
+
+
+var firstArray = Searchitem(array: [1,2,3,4,5])
+var secondArray = Searchitem(array: ["a", "b", "c"])
+
+
+firstArray.search(item: 4)
+// "Item was founded"
+
+secondArray.search(item: "d")
+// "Item wasn't founded"
 
